443d89b1465d1519078cf48b49cdf537f62358bc
AnastasiaPishaeva <n_pishaeva@mail.ru>
2026-03-06 14:48:50 +0300

    fix bug with theory on page

--- frontend/src/widgets/Content.tsx
+++ frontend/src/widgets/Content.tsx
@@ -3,10 +3,11 @@ import type { Lesson, Topic, Section } from "../entities/types";
 import { Typography } from "@mui/material";
 
 interface ContentProps {
-  section: Section;
+  section: Section | undefined;
 }
 
 const Content: React.FC<ContentProps> = ({ section }) => {
+  if (section === undefined) return <div>В уроке еще нет секций, но должна быть хоть 1!!</div>
   return (
     <>
       <Typography variant="h2">
